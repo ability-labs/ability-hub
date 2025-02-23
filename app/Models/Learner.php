daa2bf89e22bfc9c919e6a94dbb25b79a0851339
Salvo Bonanno <salvob.labs@gmail.com>
2025-02-23 14:19:25 +0100

    Attribute Casting, Accessors and Relationships for Learner Model

--- app/Models/Learner.php
+++ app/Models/Learner.php
@@ -2,12 +2,35 @@
 
 namespace App\Models;
 
+use Carbon\CarbonInterval;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Learner extends Model
 {
     /** @use HasFactory<\Database\Factories\LearnerFactory> */
-    use HasFactory, HasUuid;
+    use HasFactory, HasUuids;
+
+    protected $casts = [
+        'birth_date' => 'datetime'
+    ];
+
+    public function getAgeAttribute(): string
+    {
+        /** @var CarbonInterval $diff */
+        $diff = $this->birth_date
+            ->diff(now());
+
+        return ($diff->years >= 3) ?
+            $diff->forHumans(['parts' => 1])
+            : $diff->forHumans(['parts' => 2]);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
