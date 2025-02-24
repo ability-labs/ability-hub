f73f514d65907864461f2f5bf0eb63b7335aa157
Salvo Bonanno <salvob.labs@gmail.com>
2025-02-24 23:30:12 +0100

    Adds icons to title pages

--- resources/views/operators/create.blade.php
+++ resources/views/operators/create.blade.php
@@ -1,7 +1,11 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('New :resource', ['resource' => __('Learner')])  }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight flex items-center space-x-2">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+            </svg>
+
+            <span> {{ __('New :resource', ['resource' => __('Operator')])  }}</span>
         </h2>
     </x-slot>
 
