9eb1f5994b15a0e264683af6507a8b6075d73a40
Salvo Bonanno <salvob.labs@gmail.com>
2025-02-25 17:52:56 +0100

    Removes unused feature

--- app/Services/Datasheets/PreferenceAssessment/PairedChoice.php
+++ app/Services/Datasheets/PreferenceAssessment/PairedChoice.php
@@ -14,44 +14,4 @@ class PairedChoice extends PreferenceAssessmentAbstract
             }
         }
     }
-
-    public function generatePairings(int $sessions): array
-    {
-        if (count($this->data['items']) < 7) {
-            throw new \InvalidArgumentException("At least 7 items are required.");
-        }
-        $pairings = [];
-
-        for ($i = 0; $i < $sessions; $i++) {
-            $sessionPairs = $this->generateSessionPairs();
-            $pairings[] = $sessionPairs;
-        }
-
-        return $pairings;
-    }
-
-    protected function generateSessionPairs(): array
-    {
-        $items = $this->data['items'];
-        shuffle($items);
-
-        $pairs = [];
-        $used = [];
-
-        while (count($used) < count($items)) {
-            $pair = [];
-            foreach ($items as $item) {
-                if (!in_array($item, $used)) {
-                    $pair[] = $item;
-                    $used[] = $item;
-                    if (count($pair) == 2) {
-                        $pairs[] = $pair;
-                        break;
-                    }
-                }
-            }
-        }
-
-        return $pairs;
-    }
 }
