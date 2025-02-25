ee7422a3025a374cae3b1f78a700ba62ba50dcab
Salvo Bonanno <salvob.labs@gmail.com>
2025-02-25 08:53:30 +0100

    Adds content card to database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\ContentCard;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -14,5 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(DisciplineSeeder::class);
+        $this->call(ContentCardSeeder::class);
     }
 }
