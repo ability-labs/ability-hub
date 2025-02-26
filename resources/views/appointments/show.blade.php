fe46982ee1d8443f9bfc8e45312a405c0146795b
Salvo Bonanno <salvob.labs@gmail.com>
2025-02-26 10:27:20 +0100

    Adds breadcrumbs

--- resources/views/appointments/show.blade.php
+++ resources/views/appointments/show.blade.php
@@ -9,7 +9,19 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="max-w-6xl mx-auto  pt-4 flex items-center">
+        <x-breadcrumbs :paths="[
+            [
+                'text' => __('Appointments'),
+                'link' => route('appointments.index')
+            ],
+            [
+                'text' => $appointment->title,
+            ]
+        ]" />
+    </div>
+
+    <div class="py-4">
         <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
