2927c51a25debf1b0684aef52c52a9a0605ff450
Salvo Bonanno <salvob.labs@gmail.com>
2025-02-28 14:22:53 +0100

    Removes page header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,15 +19,6 @@
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="hidden sm:block bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
-
             <!-- Page Content -->
             <main>
                 {{ $slot }}
