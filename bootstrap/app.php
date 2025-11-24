82a993395b5c10a74241d5e49c38b35716ffe413
Salvo Bonanno <salvob.labs@gmail.com>
2025-11-24 13:52:25 +0100

    Adds sanctum and configure api routes

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,11 +9,14 @@ use Illuminate\Session\Middleware\StartSession;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->web(append: LocaleMiddleware::class);
+        $middleware
+            ->web(append: LocaleMiddleware::class)
+            ->statefulApi();
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
